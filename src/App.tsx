1064ef365a8dcf10bdb867348b4a7c89569d8c8a
dstradee <pablo.est.ram@gmail.com>
2026-09-08 14:05:35 +0200

    Create App component with layout structure

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { Navbar } from './components/layout/Navbar';
+import { Footer } from './components/layout/Footer';
+import { Hero } from './components/sections/Hero';
+import { Problem } from './components/sections/Problem';
+import { HowItWorks } from './components/sections/HowItWorks';
+import { Categories } from './components/sections/Categories';
+import { Waitlist } from './components/sections/Waitlist';
+import { Local } from './components/sections/Local';
+
+function App() {
+  return (
+    <div className="min-h-screen flex flex-col font-sans">
+      <Navbar />
+      <main className="flex-grow">
+        <Hero />
+        <Problem />
+        <HowItWorks />
+        <Categories />
+        <Waitlist />
+        <Local />
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
